505e0a427582a126e98de5458683c00e167cf58c
Frey Hargarten <harga012@umn.edu>
2018-09-17 13:48:13 -0500

    Initial commit

---
+++ app/chart.js
@@ -0,0 +1,86 @@
+import * as d3 from 'd3';
+import * as c3 from 'c3';
+
+class Chart { 
+
+  constructor(target) {
+    this.target = target;
+    this.chartCounts = null;
+  }
+
+  render() {
+    var self = this;
+
+    var  padding = {
+          top: 20,
+          right: 40,
+          bottom: 20,
+          left: 60,
+      };
+    
+    self.chartCounts = c3.generate({
+        bindto: self.target,
+        padding: padding,
+        data: {
+              // x: 'x',
+              columns: [
+                  ['2016', 1425, 19693, 35324, 45298, 98057, 124844, 206871],
+                  ['2018', 0, 0, 0, 0, 0, 0, 0]
+              ],
+          type: 'spline'
+        },
+          legend: {
+            show: false
+          },
+          point: {
+              show: true,
+              r: function(d) { if (d.x == 6) { return 6; } else { return 0; } }
+          },
+          color: {
+              pattern: ['#888888', '#333333']
+          },
+          axis: {
+                // rotated: true,
+                y: {
+                      max: 250000,
+                      min: 0,
+                      padding: {bottom: 0, top: 0},
+                      tick: {
+                       count: 4,
+                       values: [0,50000,100000,150000,200000,250000],
+                       format: d3.format(',.0f')
+                      }
+                  },
+              x: {
+                type: 'category',
+                categories: ['Sep. 1-25','Sep 26-30','Oct. 1-8','Oct. 9-15', 'Oct 16-24', 'Oct 25-31', 'Nov 1-7'],
+                padding: {right: 0, left: 0},
+                      tick: {
+                       rotate: -75,
+                       multiline: false
+                      },
+                      height: 40
+              }
+          },
+        grid: {
+            focus:{
+                show:false
+              },
+          },
+          tooltip: {
+            contents: function(d, defaultTitleFormat, defaultValueFormat, color) {
+              console.log(d);
+              return '<div class="chart-tooltip gray5">' + '<span class="tooltip-label">' + d[1].id + ':</span>' +
+                '<span class="tooltip-value">' + defaultValueFormat(d[1].value) + '</span>' +
+                '</div><div class="chart-tooltip gray3">' +
+                '<span class="tooltip-label">' + d[0].id + ':</span>' +
+                '<span class="tooltip-value">' + defaultValueFormat(d[0].value) + '</span>' +
+                '</div>';
+            }
+          }
+  });
+
+  }
+}
+
+export { Chart as default }
---
+++ app/index.js
@@ -0,0 +1,67 @@
+/**
+ * Main JS file for project.
+ */
+
+// Define globals that are added through the js.globals in
+// the config.json file, here like this:
+// /* global _ */
+
+// Utility functions, such as Pym integration, number formatting,
+// and device checking
+
+//import utilsFn from './utils.js';
+//utilsFn({ });
+
+
+// Import local ES6 modules like this:
+//import utilsFn from './utils.js';
+
+// Or import libraries installed with npm like this:
+// import module from 'module';
+
+// Utilize templates on the client.  Get the main content template.
+//import Content from '../templates/_index-content.svelte.html';
+//
+// Get the data parts that are needed.  For larger data points,
+// utilize window.fetch.  Add the build = true option in the buildData
+// options.
+//import content from '../content.json';
+// OR: let content = await (await window.fetch('./assets/data/content.json')).json();
+//
+// const app = new Content({
+//   target: document.querySelector('.main-app-container'),
+//   data: {
+//     content
+//   }
+// });
+
+'use strict';
+
+// Dependencies
+import utilsFn from './utils.js';
+import Chart from './chart.js';
+import barChart from './barchart.js';
+import ageChart from './agechart.js';
+import regionChart from './regionchart.js';
+import Map from './map.js';
+
+// Import local ES6 modules like this:
+//import utilsFn from './utils.js';
+
+// Or import libraries installed with npm like this:
+// import module from 'module';
+
+// Setup utils function
+utilsFn({ });
+
+const chart = new Chart('#chart');
+const chart2 = new regionChart('#chartRegion');
+const chart3 = new ageChart('#chartAge');
+const chart4 = new barChart('#chartPrimary');
+const map = new Map("#mapCounties");
+
+chart.render();
+chart2.render();
+chart3.render();
+chart4.render();
+map.render();
