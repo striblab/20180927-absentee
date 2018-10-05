85566cafd8ed9e77a562b1ff03f9c1d3033b6ff4
Chase Davis <chase.davis@gmail.com>
2018-10-05 16:55:05 -0500

    Kill me

--- app/index.js
+++ app/index.js
@@ -45,6 +45,7 @@ import barChart from './barchart.js';
 import ageChart from './agechart.js';
 import regionChart from './regionchart.js';
 import Map from './map.js';
+import Odometer from 'odometer';
 
 // Import local ES6 modules like this:
 //import utilsFn from './utils.js';
@@ -55,34 +56,48 @@ import Map from './map.js';
 // Setup utils function
 utilsFn({ });
 
-import Content from '../templates/_index-content.svelte.html';
-//
-// Get the data parts that are needed.  For larger data points,
-// utilize window.fetch.  Add the build = true option in the buildData
-// options.
-import content from '../content.json';
-// OR: let content = await (await window.fetch('./assets/data/content.json')).json();
-//
-const app = new Content({
-  target: document.querySelector('.main-app-container'),
-  data: {
-    content
-  }
-});
+// import Content from '../templates/_index-content.svelte.html';
+// //
+// // Get the data parts that are needed.  For larger data points,
+// // utilize window.fetch.  Add the build = true option in the buildData
+// // options.
+// import content from '../content.json';
+// // OR: let content = await (await window.fetch('./assets/data/content.json')).json();
+// //
+// const app = new Content({
+//   target: document.querySelector('.main-app-container'),
+//   data: {
+//     content
+//   }
+// });
 
-const chart0 = new Chart('#chart');
-const chart1 = new compareChart('#chartYear');
-const chart2 = new regionChart('#chartRegion');
-const chart3 = new ageChart('#chartAge');
-const chart4 = new barChart('#chartPrimary');
-const map = new Map("#mapCounties");
+(function(){
+    const chart0 = new Chart('#chart');
+    const chart1 = new compareChart('#chartYear');
+    const chart2 = new regionChart('#chartRegion');
+    const chart3 = new ageChart('#chartAge');
+    const chart4 = new barChart('#chartPrimary');
+    const map = new Map("#mapCounties");
 
-chart0.render();
-chart1.render();
-chart2.render();
-chart3.render();
-chart4.render();
-map.render();
+    chart0.render();
+    chart1.render();
+    chart2.render();
+    chart3.render();
+    chart4.render();
+    map.render();
+
+    const VOTE_COUNT = 42552;
+
+    // Make odometer
+    let el = document.querySelector('.odometer');
+
+    let od = new Odometer({
+      el: el,
+      value: 0
+    });
+
+    od.update(VOTE_COUNT)
+})();
 
 // setTimeout(function(){
 //     odometer.update(42552)
